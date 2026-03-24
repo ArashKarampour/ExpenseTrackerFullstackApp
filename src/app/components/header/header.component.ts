5dce75d3c93ced968a2d878024c79715df81e33e
Arash Karampour <karampour.arash@yahoo.com>
2026-03-24 17:41:08 +0100

    feat: adding behaviorsubject to control the user's state for controling user login and logout buttons in the header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  constructor(public authService: AuthService) { }
 }
